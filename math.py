7778fd6a3276b090b1468b01acb69dd027b6d805
AbhishekKakade <71581315+AbhishekKakade@users.noreply.github.com>
2022-01-27 17:47:49 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+//add implementation
+def add(x,y):
+	return x+y
+    
+//subtract implementation	
+def Subtract(x,y):
+	return x-y              #on master 
+    
+//mul implementation	
+def mul(x,y):
+	return x*y          #on bug456
+    
+//divide implementation	
+def divide(x,y):        #on master
+	if y==0
+        return divide_by_zero_error
+    else 
+        return x/y
