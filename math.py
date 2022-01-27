c1c4522057e100b31776c065ee607af9277cfb6a
Abhishek Kakade <abhishek_kakade@persistent.com>
2022-01-27 17:52:57 +0530

    Added + comment

--- math.py
+++ math.py
@@ -1,16 +1,16 @@
-//add implementation
+//add implementation + adding extra cooment
 def add(x,y):
 	return x+y
     
-//subtract implementation	
+//subtract implementation	+ adding extra cooment
 def Subtract(x,y):
 	return x-y              #on master 
     
-//mul implementation	
+//mul implementation	+ adding extra cooment
 def mul(x,y):
 	return x*y          #on bug456
     
-//divide implementation	
+//divide implementation	 + adding extra cooment
 def divide(x,y):        #on master
 	if y==0
         return divide_by_zero_error
